db8d1d5bbdf6acc167529874710cf46037645406
jeffssant <santana.jeff@gmail.com>
2021-05-01 16:03:12 -0300

    Iniciando sidenav e tollbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list'; 
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 
@@ -8,10 +9,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({ 
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
