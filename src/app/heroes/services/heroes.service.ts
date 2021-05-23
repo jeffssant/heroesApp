993c0d9ccb5ce7f99d293872d709df4329877c5a
jeffssant <santana.jeff@gmail.com>
2021-05-23 10:55:14 -0300

    Recebendo dados de heroe

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Injectable({
@@ -11,5 +12,9 @@ export class HeroesService {
 
   getHeroes() {
     return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+  }
+
+  getHeroesPorId(id:string):Observable<Heroe> {
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
   }
 }
