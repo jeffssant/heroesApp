c84f08af0ba6c9607bc9de7fbed53e9918eb280b
jeffssant <santana.jeff@gmail.com>
2021-05-24 10:54:25 -0300

    Pagina detalhe de heoroi

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Heroe } from '../../interfaces/heroes.interface';
 import { switchMap } from "rxjs/operators";
 import { HeroesService } from '../../services/heroes.service';
@@ -8,6 +8,12 @@ import { HeroesService } from '../../services/heroes.service';
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: [
+    `
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+    `
   ]
 })
 export class HeroeComponent implements OnInit {
@@ -15,7 +21,8 @@ export class HeroeComponent implements OnInit {
   heroe!: Heroe;
 
   constructor(private activatedRoute: ActivatedRoute, 
-              private heroesService: HeroesService) { }
+              private heroesService: HeroesService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
@@ -23,6 +30,10 @@ export class HeroeComponent implements OnInit {
       switchMap(({id}) => this.heroesService.getHeroesPorId(id))
       )
     .subscribe(heroe => this.heroe = heroe)
+  }
+
+  regressar(){
+    this.router.navigate(['/heroes/listado'])
   }
 
 }
