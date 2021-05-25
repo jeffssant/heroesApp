833876e30ac02875d73f9dfdd2c873fe77660865
jeffssant <santana.jeff@gmail.com>
2021-05-25 17:16:50 -0300

    apagar imagem

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -75,4 +75,10 @@ export class AgregarComponent implements OnInit {
     
   }
 
+  borrarHeroe() {
+    this.heroesService.borrarHeroe(this.heroe.id!).subscribe( resp => {
+      this.router.navigate(['/heroes/listado'])
+    })
+  }
+
 }
